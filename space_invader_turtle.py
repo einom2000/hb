dd20de2952f3e47bd677c608e938861296a0fee2
einom.ng@gmail.com <starBB002347>
2019-03-27 20:02:35 +0800

    111

--- space_invader_turtle.py
+++ space_invader_turtle.py
@@ -1,6 +1,7 @@
 import turtle
 import os
-
+import random
+import math
 
 wn = turtle.Screen()
 wn.setup(600, 600)
@@ -44,7 +45,7 @@ for i in range(5):
     enemy.shape('circle')
     enemy.penup()
     enemy.speed(0)
-    enemy.setposition(-150 + i * (int((600 - 300 - 20) / 5) - 20), 250)
+    enemy.setposition(random.randint(-250, 250), random.randint(100, 250))
     print(i * (int((600 - 300 - 60) / 5) - 60))
     enemies.append(enemy)
 
@@ -81,14 +82,23 @@ def move_right():
     player.setx(x)
 
 def fire_bullelt():
-    global bulletsate
+    global bulletstate
+    if bulletstate == 'ready':
+        x = player.xcor()
+        y = player.ycor() + 10
+        bullet.setposition(x, y)
+        bullet.showturtle()
+        bulletstate = 'on_fire'
 
-    x = player.xcor()
-    y = player.ycor() + 10
-    bullet.setposition(x, y)
-    bullet.showturtle()
-
-
+def is_collision(objecta, objectb):
+    ax = objecta.xcor()
+    ay = objecta.ycor()
+    bx = objectb.xcor()
+    by = objectb.ycor()
+    if math.sqrt(math.pow((ax - bx), 2) + math.pow((ay - by), 2)) <= 15:
+        return True
+    else:
+        return False
 # Create keyboard bindings
 wn.listen()
 wn.onkey(move_left, 'Left')
@@ -96,6 +106,14 @@ wn.onkey(move_right, 'Right')
 wn.onkey(fire_bullelt, 'space')
 
 while True:
+    if bulletstate == 'on_fire':
+        y = bullet.ycor()
+        y += bulletspeed
+        bullet.sety(y)
+        if y >= 280:
+            bullet.hideturtle()
+            bullet.sety(-350)
+            bulletstate = 'ready'
     # Move the enemy back and down
     for enemy in enemies:
         x = enemy.xcor()
@@ -107,10 +125,20 @@ while True:
                 y -= 40
                 every_enemy_in_role.sety(y)
             enemyspeed *= -1
+        if is_collision(enemy, bullet):
+            bullet.hideturtle()
+            bullet.sety(-350)
+            bulletstate = 'ready'
+            enemy.hideturtle()
+            index = enemies.index(enemy)
+            enemies.pop(index)
+    if len(enemies) == 0:
+        win = turtle.Turtle()
+        win.pensize(4)
+        win.color('red')
+        win.write('YOU WIN !')
 
-    y = bullet.ycor()
-    y += bulletspeed
-    bullet.sety(y)
+
     #
 
     wn.update()
